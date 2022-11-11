2d3b053c246f1e2aca1e269726f1f71cf2e6e617
Nahia Riviera <n.riviera.dev@gmail.com>
2022-11-11 16:35:12 -0600

    added cors to accountcontroller

--- BankingSystem_BackEnd/src/main/java/com/Revature/controller/AccountController.java
+++ BankingSystem_BackEnd/src/main/java/com/Revature/controller/AccountController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.Revature.model.Account;
@@ -17,6 +18,7 @@ import com.Revature.model.User;
 import com.Revature.service.AccountService;
 import com.Revature.service.UserService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @Controller
 public class AccountController {
 	@Autowired
